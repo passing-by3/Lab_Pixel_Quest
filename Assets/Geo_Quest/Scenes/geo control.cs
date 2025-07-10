39da87e573d692f67a24ceedb8ed84750dc25327
passing-by3 <po.the.potato2014@gmail.com>
2025-07-10 12:14:47 -0400

    hhh

--- Assets/Geo_Quest/Scenes/geo control.cs
+++ Assets/Geo_Quest/Scenes/geo control.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class geocontrol : MonoBehaviour
 {
-    int var = 3;
-    string varOne = "Hello ";
+    private Rigidbody2D rb;
+    public int speed = 3;
     // Start is called before the first frame update
     void Start()
     {
-        string varTwo = "World";
-     Debug.Log(varOne + varTwo);
+        rb = GetComponent<Rigidbody2D>();
+     
         
         
     }
@@ -18,8 +18,23 @@ public class geocontrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var);
-        var++;
+        float xSpeed = Input.GetAxis("Horizontal");
+        Debug.Log("xSpeed");
+        rb.velocity = new Vector2(xSpeed * speed, rb.velocity.y);
 
+        
+        /*if (Input.GetKeyDown(KeyCode.W))
+        {transform.position += new Vector3(0, 1, 0);}
+        if (Input.GetKeyDown(KeyCode.S))
+        {transform.position += new Vector3(0, -1, 0);}
+        if (Input.GetKeyDown(KeyCode.D))
+        {transform.position += new Vector3(1, 0, 0);}
+        if (Input.GetKeyDown(KeyCode.A))
+        {transform.position += new Vector3(-1, 0, 0);}*/
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
     }
 }
