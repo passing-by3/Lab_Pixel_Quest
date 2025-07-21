ed6d9792d9781098b99803bba4b16b88d094d534
passing-by3 <po.the.potato2014@gmail.com>
2025-07-21 11:56:29 -0400

    jkjk

--- Assets/Pixel_Quest/Scripts/player jump.cs
+++ Assets/Pixel_Quest/Scripts/player jump.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class playerjump : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float jumpForce = 10;
+    public float fallForce = -1;
+    private Vector2 gf;
+
+    public float capsuleHeight = .25f;
+    public float capsuleRadius = .08f;
+
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool gc;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gf = new Vector2(0, Physics2D.gravity.y);
+        rb  = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        gc = Physics2D.OverlapCapsule(feetCollider.position, new Vector2(capsuleHeight, capsuleRadius), CapsuleDirection2D.Horizontal, 0, groundMask);
+
+        if (Input.GetKeyDown(KeyCode.Space) && gc)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(rb.velocity.y<0)
+        {
+            rb.velocity += gf * (fallForce * Time.deltaTime);
+        }
     }
 }
